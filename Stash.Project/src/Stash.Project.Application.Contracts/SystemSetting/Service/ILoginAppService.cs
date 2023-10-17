8e7865779a68a702f52be05a9e2f5eec6e633397
王圳垚 <lenovo@IPHONE>
2023-10-17 14:43:29 +0800

    10.17登录

---
+++ Stash.Project/src/Stash.Project.Application.Contracts/SystemSetting/Service/ILoginAppService.cs
@@ -0,0 +1,19 @@
+﻿using Stash.Project.SystemSetting.Dto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+
+namespace Stash.Project.SystemSetting.Service
+{
+    public interface ILoginAppService : IApplicationService
+    {
+        /// <summary>
+        /// 登录
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        Task<ApiResult> LoginShow(LoginDto obj);
+    }
+}
---
+++ Stash.Project/src/Stash.Project.Application/SystemSetting/Service/LoginAppService.cs
@@ -0,0 +1,51 @@
+﻿using Stash.Project.SystemSetting.Dto;
+using Stash.Project.Stash.SystemSetting.Model;
+using Stash.Project.SystemSetting.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace Stash.Project.SystemSetting.Service
+{
+    public class LoginAppService : ApplicationService, ILoginAppService
+    {
+        private readonly IRepository<UserInfo, long> _repository;
+
+        public LoginAppService(IRepository<UserInfo, long> repository)
+        {
+            _repository = repository;
+        }
+        /// <summary>
+        /// 登录
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public async Task<ApiResult> LoginShow(LoginDto obj)
+        {
+            var list = await _repository.FirstOrDefaultAsync(x => x.User_Name == obj.uname && x.User_Password == obj.pwd);
+
+            if (list == null)
+            {
+                return new ApiResult
+                {
+                    code = ResultCode.Error,
+                    data = list,
+                    msg = ResultMsg.RequestError,
+                    count = 0
+                };
+            }
+            return new ApiResult
+            {
+                code = ResultCode.Success,
+                data = list,
+                msg = ResultMsg.RequestSuccess,
+                count = 0
+            };
+        }
+    }
+}
