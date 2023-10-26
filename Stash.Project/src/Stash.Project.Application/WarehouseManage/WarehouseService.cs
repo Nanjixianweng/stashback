24f6a6f643edcfe92d04a05261dd6c8c11ef5662
王圳垚 <lenovo@IPHONE>
2023-10-26 11:45:34 +0800

    10.26入库

--- Stash.Project/src/Stash.Project.Application/WarehouseManage/WarehouseService.cs
+++ Stash.Project/src/Stash.Project.Application/WarehouseManage/WarehouseService.cs
@@ -11,6 +11,8 @@ using Microsoft.AspNetCore.Mvc;
 using Yitter.IdGenerator;
 using Stash.Project.IWarehouseManage.WarehouseDto;
 using Volo.Abp.ObjectMapping;
+using Stash.Project.Stash.BasicData.Model;
+using Stash.Project.Stash.BusinessManage.Model;
 
 namespace Stash.Project.WarehouseManage
 {
@@ -39,14 +41,26 @@ namespace Stash.Project.WarehouseManage
         /// 单据类型表
         /// </summary>
         private readonly IRepository<DocumentType> _documentType;
+        /// <summary>
+        /// 库位表
+        /// </summary>
+        private readonly IRepository<StorageLocationTable> _storageLocation;
+        /// <summary>
+        /// 销售单状态
+        /// </summary>
+        private readonly IRepository<PurchaseProductRelationshipTable> _purchaseProduct;
 
-        public WarehouseService(IRepository<StashProductTable> stashPro, IRepository<PutStorageTable> putStor, IRepository<OutStorageTable> outStor, IRepository<PutStorageStateTable> warState, IRepository<DocumentType> documentType)
+
+
+        public WarehouseService(IRepository<StashProductTable> stashPro, IRepository<PutStorageTable> putStor, IRepository<OutStorageTable> outStor, IRepository<PutStorageStateTable> warState, IRepository<DocumentType> documentType, IRepository<StorageLocationTable> storageLocation, IRepository<PurchaseProductRelationshipTable> purchaseProduct)
         {
             _stashPro = stashPro;
             _putStor = putStor;
             _outStor = outStor;
             _warState = warState;
             _documentType = documentType;
+            _storageLocation = storageLocation;
+            _purchaseProduct = purchaseProduct;
         }
 
         /// <summary>
@@ -82,8 +96,12 @@ namespace Stash.Project.WarehouseManage
                 ints += random.Next(0, 9);
             }
 
-            var rodNum = DateTime.Now.ToString("yyyyMMddHHmmss")+ ints;
+            //批次生成规则:年月日时分秒+随机数*4
+            var rodNum = DateTime.Now.ToString("yyyyMMddHHmmss") + ints;
 
+            var defuid = _storageLocation.FirstOrDefaultAsync(x => x.DefaultrorNot == true).Id;
+
+            //关系表
             foreach (var item in obj.stashProductDto)
             {
                 item.Id = YitIdHelper.NextId();
@@ -91,13 +109,16 @@ namespace Stash.Project.WarehouseManage
                 item.PutStorage_Id = obj.putStorageDto.Id;
 
                 item.PutStorage_Lot = rodNum;
+
+                item.PutStorage_Position = defuid.ToString();
             }
 
+            //入库表
             PutStorageTable putStorageTable = new PutStorageTable();
 
-           var flies= putStorageTable.GetType().GetProperty("Id");
+            var flies = putStorageTable.GetType().GetProperty("Id");
 
-            if (flies!=null)
+            if (flies != null)
             {
                 flies.SetValue(putStorageTable, obj.putStorageDto.Id);
             }
@@ -109,12 +130,21 @@ namespace Stash.Project.WarehouseManage
 
             await _putStor.InsertAsync(putStorageTable);
 
-          var info=  ObjectMapper.Map<List<StashProductDto>,List<StashProductTable>>(obj.stashProductDto);
+            var info = ObjectMapper.Map<List<StashProductDto>, List<StashProductTable>>(obj.stashProductDto);
 
             await _stashPro.InsertManyAsync(info);
 
-            return new ApiResult() { 
-             code = 200,
+            //更改采购状态
+            var stateList = (await _purchaseProduct.ToListAsync()).WhereIf(putStorageTable.PutStorage_OrderId != null, x => x.PurchaseId == putStorageTable.PutStorage_OrderId);
+
+            foreach (var item in stateList)
+            {
+                item.Status = Stash.TableStatus.PurchaseProductRelationshipStatus.全部入库;
+            }
+
+            return new ApiResult()
+            {
+                code = 200,
             };
         }
     }
